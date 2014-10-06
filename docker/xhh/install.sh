abfd3fb24dbb1f464bf84e177df9c27330a328e4
Vitaly Glibin <glibin.v@gmail.com>
2014-10-06 15:19:18 +0400

    add static for xhh

--- docker/xhh/install.sh
+++ docker/xhh/install.sh
@@ -6,6 +6,7 @@ then
     curl -o ~/.boot2docker/boot2docker.iso http://static.dockerfiles.io/boot2docker-v1.2.0-virtualbox-guest-additions-v4.3.14.iso
     VBoxManage sharedfolder add boot2docker-vm -name home -hostpath /Users
     VBoxManage modifyvm boot2docker-vm --natpf1 "xhh,tcp,,9300,,9300"
+    VBoxManage modifyvm boot2docker-vm --natpf1 "xhh_static,tcp,,9301,,9301"
     boot2docker up
 fi
 
--- docker/xhh/run.sh
+++ docker/xhh/run.sh
@@ -3,4 +3,4 @@
 DOCROOT=${DOCROOT:-"/Users/belk/Documents/projects/hh.sites.main"}
 
 docker rm xhh
-docker run -p 9300:9300 -t -i --rm="true" --name="xhh" -v ${DOCROOT}:/data glibin.pyn.ru:5000/xhh
+docker run -p 9300:9300 -p 9301:9301 -t -i --rm="true" --name="xhh" -v ${DOCROOT}:/data glibin.pyn.ru:5000/xhh
